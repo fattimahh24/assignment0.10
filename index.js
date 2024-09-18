74ca6f35a98fdd9758000ae4df2f40d842e2db97
Fatima Munawar <fattimahh24@gmail.com>
2024-09-18 17:29:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Qno:01
+var name = "fatima";
+alert(name);
+
+// Qno: 02
+var fullname = "fatima munawar"
+console.log ("my name is" +" "+ fullname);
+
+// Qno: 03
+var a = 4
+var b = 2
+console.log (a++ + --b - --a + b++ + a + b)
+
+// Qno: 04 
+var Qno1 = prompt("are you a student at SMIT?");
+var Qno2 = prompt("is your attendence is complete?");
+if (Qno1 === "yes" && Qno2 === "yes"){
+    alert("You are able to attend the class");
+}
+else if ( Qno2 === "no" && Qno2 === "no"){
+    alert("You are not able to attend the class");
+}
